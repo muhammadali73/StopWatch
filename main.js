81ce8ae00411568c02b722a53ee9866ce7bb7534
muhammad ali <muhammadali30302@gmail.com>
2024-03-21 16:51:31 +0500

    OK

--- main.js
+++ main.js
@@ -1,44 +1,53 @@
 let timerInterval;
-function on(){
-document.getElementById("alert").innerHTML = "You Are Online";
-}
-function off(){
-document.getElementById("alert").innerHTML = "You Are Offline";
-}
-function start(){
-    document.getElementById("start").disabled = true;
 let second = 0
-    let minute = 0
-    let hour = 0
+let minute = 0
+let hour = 0
+let stoppedTime = 0;
+function on() {
+    document.getElementById("alert").innerHTML = "You Are Online";
+}
+function off() {
+    document.getElementById("alert").innerHTML = "You Are Offline";
+}
+function start() {
+    document.getElementById("start").disabled = true;
 
-    function timer(){
+    function timer() {
         second++
-        if(second===60){
+        if (second === 60) {
             minute++;
             second = 0;
         }
-        if(minute===60){
+        if (minute === 60) {
             hour++;
-            minute=0;
+            minute = 0;
         }
-    updateDisplay();
+        updateDisplay();
     }
-    function updateDisplay(){
+    function updateDisplay() {
         const timer = document.getElementById("timer");
         timer.textContent = hour + "0 : 0" + minute + " : " + "0" + second;
-        if(second>9){
+        if (second > 9) {
             timer.textContent = hour + "0 : 0" + minute + " : " + second;
         }
-        if(minute>9){
+        if (minute > 9) {
             timer.textContent = hour + "0 : " + minute + " : " + "0" + second;
         }
     }
-    timerInterval = setInterval(timer,1000)}
-    function stop(){
-        clearInterval(timerInterval)
-    }
-    function reset(){
-        clearInterval(timerInterval);
-        document.getElementById("timer").textContent = "00 : 00 : 00";
-        document.getElementById("reset").disabled = false;
-    }
+    timerInterval = setInterval(timer, 1000)
+}
+function stop() {
+    clearInterval(timerInterval)
+    document.getElementById("start").disabled = false;
+    stoppedTime = Date.now() - (hour * 60 * 60 * 1000 + minute * 60 * 1000 + second * 1000);
+}
+function reset() {
+    clearInterval(timerInterval);
+    second = 0;
+    minute = 0;
+    hour = 0;
+    stoppedTime = 0;
+    document.getElementById("timer").textContent = "00 : 00 : 00";
+    document.getElementById("reset").disabled = false;
+    document.getElementById("start").disabled = false;
+}
